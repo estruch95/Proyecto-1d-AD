2677f1a01ab746f202bb5408102400ab1c8ab6ef
estruch18 <estruch18@192.168.1.135>
2015-09-30 23:32:09 +0200

    Finalización Proyecto 1d AD
    Signed-off-by: estruch18 <estruch18@192.168.1.135>

--- Proyecto1d/src/Funciones/FuncionesImpl.java
+++ Proyecto1d/src/Funciones/FuncionesImpl.java
@@ -20,7 +20,7 @@ public class FuncionesImpl {
 		
 		/*El método ordenarFichero recibe 3 parámetros, un fichero origen, un fichero destino y un tercer parámetro 
 		donde indicamos con 0 si el orden es natural o con 1 si es inverso.*/
-		this.ordenarFichero(f1,f3,1);
+		this.ordenarFichero(f1,f3,0);
 	}
 	
 	public void ordenarFichero(File origen, File destino, int tipoOrden){
@@ -53,33 +53,41 @@ public class FuncionesImpl {
 			bfrOrigen.close();
 			
 			/*Una vez se ha leido todo el fichero, se almacenan dichas lineas en el ArrayList "ficheroOrigen" y posteriormente lo condicionamos:
-			  Si el tipoOrden introducido es 0, se recorrerá el ArrayList de forma normal.
-			  Si el tipoOrden introducido es 1, se recorrerá el ArrayList de forma inversa.
+			  Segun el tipoOrden introducido, si es 0, se recorrerá el ArrayList de forma normal.
+			  Segun el tipoOrden introducido, si es 1, se recorrerá el ArrayList de forma inversa.
  		   */
 			
-			if(tipoOrden == 0){
-				//Escritura en fichero destino con orden natural
-				for(int a=0;a<ficheroOrigen.size();a++){
-					bfwDestino.write(ficheroOrigen.get(a)+"\n");
-				}
-				//Info. consola
-				System.out.println("Copiado correctamente en orden natural.");
+			//Si el tipo de orden pasado por parámetro es diferente de 0 y 1 se mostrará un error
+			if(tipoOrden>1){
+				System.err.println("Tipo de orden no válido, 0 si desea orden natural o 1 inverso.");
 			}
-			else if (tipoOrden == 1){
-				//Escritura en fichero destino con orden inverso
-				for(int a=(ficheroOrigen.size()-1);a>=0;a--){
-					bfwDestino.write(ficheroOrigen.get(a)+"\n");
-				}
-				//Info. consolas
-				System.out.println("Copiado correctamente en orden inverso.");
+			else{
+				
+				switch(tipoOrden){
+				case 0:
+					//Escritura en fichero destino con orden natural
+					for(int a=0;a<ficheroOrigen.size();a++){
+						bfwDestino.write(ficheroOrigen.get(a)+"\n");
+					}
+					//Info. consola
+					System.out.println("Copiado correctamente en orden natural.");
+					break;
+				case 1:
+					//Escritura en fichero destino con orden inverso
+					for(int a=(ficheroOrigen.size()-1);a>=0;a--){
+						bfwDestino.write(ficheroOrigen.get(a)+"\n");
+					}
+					//Info. consolas
+					System.out.println("Copiado correctamente en orden inverso.");
+					break;
 			}
 			//Cerramos el buffer de escritura
 			bfwDestino.close();
-			
+			}
 		}
 		catch(IOException error){
+			//En caso de error, informamos acerca de este
 			System.err.println("Error al ordenar el texto al fichero destino.");
 		}
 	}
-
 }
